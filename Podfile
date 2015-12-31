8e0052ca60f5a2f0c3d45853f2aee037e9f54379
Jordan PR Olson <Jjpprroo15@gmail.com>
2015-12-30 21:42:05 -0600

    Real first commit

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'iOSChallenge1230' do
+
+pod 'Alamofire', '~> 3.0'
+pod 'Charts', '~> 2.1'
+pod 'pop', '~> 1.0'
+
+
+end
+
---
+++ Pods/Target Support Files/Pods-iOSChallenge1230/Pods-iOSChallenge1230-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_iOSChallenge1230VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iOSChallenge1230VersionString[];
+
